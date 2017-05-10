434953d2f6b8c74a6662a4b470cdb02c1b2f5a9d
Andrew Salcedo <asalcedo07@gmail.com>
2017-05-10 09:57:51 -0400

    add event form in html. begin new Vue in app.js, todo: methods

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+new Vue({
 
+      //we want to target the div with an id='events
+      el: '#events',
+
+      //here we can register any values/collections that hold data
+      //for the application
+      data: {
+          event: { name: '', description: '', date: '' },
+          events: []
+      },
+
+      //anything within the ready function will run when the app loads
+      mounted: function() {
+          //when the app loads, we want to call the method that initializes
+          //some ddata
+          this.fetchEvents();
+      },
+
+      //methods we want to use in our app are registered here
+      methods: {}
+});
