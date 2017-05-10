6397580544a326d307949d92cbe9ad880fe5effa
Andrew Salcedo <asalcedo07@gmail.com>
2017-05-10 10:18:24 -0400

    add event data in app.js, listing area index.html. Done

--- app.js
+++ app.js
@@ -18,5 +18,45 @@ new Vue({
       },
 
       //methods we want to use in our app are registered here
-      methods: {}
+      methods: {
+          //we dedicate a method to retrieving and setting some data
+          fetchEvents: function() {
+              var events = [
+                {
+                  id: 1,
+                  name: 'TIFF',
+                  description: 'Toronto International Film Festival',
+                  date: '2017-09-10'
+                },
+                {
+                  id: 2,
+                  name: 'Star Wars The Last Jedi Premiere',
+                  description: 'Luke Skywalker is back baby!',
+                  date: '2017-12-15'
+                },
+                {
+                  id: 3,
+                  name: 'SXSW',
+                  description: 'Music, film, and interactive festival in Austin, TX.',
+                  date: '2017-3-11'
+                }
+              ];
+
+              //set the collection of events
+              this.events = events;
+          },
+
+          addEvent: function() {
+              if(this.event.name) {
+                  this.events.push(this.event);
+                  this.event = {name: '', description: '', date: '' };
+              }
+          },
+
+          deleteEvent: function(index) {
+              if(confirm("Are you sure you want to delete this event?")) {
+                  this.events.splice(index, 1);
+              }
+          }
+      }
 });
